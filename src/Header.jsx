c375b359135e92f2798e4471a129c2ff80ee3951
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-31 21:39:38 +0530

    add header

---
+++ src/Header.jsx
@@ -0,0 +1,30 @@
+import { Menu } from 'lucide-react';
+
+function Header() {
+  return (
+    <div className="border-b border-gray-100 bg-white shadow-sm">
+      <div className="mx-auto flex max-w-7xl items-center justify-between p-5">
+        <div>
+          <img
+            className="w-32 md:w-36"
+            src="https://zerodha.com/static/images/logo.svg"
+            alt="logo-img"
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="hidden gap-10 text-stone-600 md:flex md:items-center">
+            <a href="/">Signup</a>
+            <a href="/">About</a>
+            <a href="/">Products</a>
+            <a href="/">Pricing</a>
+            <a href="/">Support</a>
+          </div>
+          <Menu className="ml-8 cursor-pointer" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/Zerodha.jsx
+++ src/Zerodha.jsx
@@ -1,5 +1,11 @@
+import Header from './Header';
+
 function Zerodha() {
-  return <div>UI Kitchen</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Zerodha;
