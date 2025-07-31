f58fa794ef782d18176ab8d28ba2aa23fbb842e4
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-31 22:14:34 +0530

    add hero section

---
+++ src/HeroSection.jsx
@@ -0,0 +1,25 @@
+function HeroSection() {
+  return (
+    <div className="mx-auto flex h-screen max-w-7xl flex-col items-center">
+      <img
+        className="mt-15 md:mt-30 lg:w-9/12"
+        src="https://zerodha.com/static/images/landing.png"
+        alt="hero-img"
+      />
+
+      <h1 className="mt-14 text-3xl font-semibold text-stone-600">
+        Invest in everything
+      </h1>
+      <p className="mt-4 text-center text-lg text-stone-600">
+        Online platform to invest in stocks, derivatives, mutual funds, ETFs,
+        bonds, and more.
+      </p>
+
+      <button className="mt-12 h-10 w-42 rounded-sm bg-[#387ED1] text-lg font-semibold text-white hover:bg-black">
+        Sign up for free
+      </button>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/Zerodha.jsx
+++ src/Zerodha.jsx
@@ -1,9 +1,11 @@
 import Header from './Header';
+import HeroSection from './HeroSection';
 
 function Zerodha() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
