0060fb59e684ba1dab3ad7969104a25b6c94a414
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-02 18:33:15 +0530

    fix bug in app

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,6 @@ export default function App() {
           <Link to="/chips-input" className="block w-full">
             <ComponentItem itemName="Chips Input" />
           </Link>
-          <Link to="/array-to-zigzag-string-converter" className="block w-full">
-            <ComponentItem itemName="Array to Zigzag String Converter" />
-          </Link>
         </div>
       </div>
     </div>
