ced3f63aab7bbbec36d66986efff6d8269479faa
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-02 12:38:58 +0530

    build app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import ComponentItem from './ComponentItem';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="min-h-screen w-full bg-black">
+      <div className="mx-auto h-screen max-w-5xl">
+        <h1 className="bg-gradient-to-b from-neutral-50 to-neutral-600 bg-clip-text pt-4 pb-6 text-center font-mysoul text-7xl font-semibold text-transparent">
+          Component Kitchen
+        </h1>
+
+        <div className="flex flex-col items-center gap-6">
+          <ComponentItem itemName="Chips Input" />
+          <ComponentItem itemName="Array to Zigzag String Converter" />
+        </div>
+      </div>
+    </div>
+  );
 }
