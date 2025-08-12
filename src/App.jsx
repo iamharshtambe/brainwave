3e5dc7ab51dd2b3a239db4eabb5283f9137293cd
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-12 18:44:35 +0530

    remove compoenents

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ export default function App() {
   return (
     <div className="min-h-screen w-full bg-black">
       <div className="mx-auto h-screen max-w-5xl">
-        <h1 className="bg-gradient-to-b from-neutral-50 to-neutral-600 bg-clip-text pt-4 pb-6 text-center font-mysoul text-7xl font-semibold text-transparent">
+        <h1 className="cursor-pointer bg-gradient-to-b from-neutral-50 to-neutral-600 bg-clip-text pt-4 pb-6 text-center font-mysoul text-7xl font-semibold text-transparent">
           Component Kitchen
         </h1>
 
         <div className="flex flex-col items-center gap-6">
-          <Link to="/accordian" className="block w-full">
+          {/* <Link to="/accordian" className="block w-full">
             <ComponentItem itemName="Accordian" />
-          </Link>
+          </Link> */}
         </div>
       </div>
     </div>
--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
-import Accordian from './components/Accordian.jsx';
 
 export const router = createBrowserRouter([
   { path: '/', element: <App /> },
-  { path: '/accordian', element: <Accordian /> },
+  // { path: '/accordian', element: <Accordian /> },
 ]);
