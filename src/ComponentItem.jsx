d779729b2eef6e69961bb64279b71ea1921c71c3
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-23 13:52:07 +0530

    fix margins

--- src/ComponentItem.jsx
+++ src/ComponentItem.jsx
@@ -2,7 +2,7 @@ import { ArrowRight } from 'lucide-react';
 
 export default function ComponentItem({ itemName }) {
   return (
-    <div className="relative flex w-full transform cursor-pointer items-center justify-between rounded-full bg-white p-6 text-xl font-semibold transition duration-300 hover:scale-102">
+    <div className="relative mb-6 flex w-full transform cursor-pointer items-center justify-between rounded-full bg-white p-6 text-xl font-semibold transition duration-300 hover:scale-102">
       <div className="absolute inset-x-0 -bottom-px h-[2px] w-full bg-gradient-to-r from-transparent via-red-600 to-transparent"></div>
 
       <h2 className="hover:text-red-500">{itemName}</h2>
