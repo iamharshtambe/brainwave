2203b2b487122a4b75c9f6183e96f5d8bd6464d9
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-27 10:55:54 +0530

    feat: add progress according to percentages

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,8 +1,23 @@
+const progressBars = [20, 40, 60, 80, 100];
+
 export default function ProgressBar() {
   return (
     <div className="mx-auto flex min-h-screen items-center justify-center bg-black text-white">
       <div className="flex flex-col">
         <h1 className="p-4 text-center font-bitcount text-4xl">Progress Bar</h1>
+
+        <div>
+          {progressBars.map((progressValue) => (
+            <div className="my-3 w-[600px] overflow-hidden rounded-2xl border-2 border-white bg-white text-black">
+              <div
+                style={{ width: `${progressValue}%` }}
+                className="bg-blue-500 px-2"
+              >
+                {progressValue}%
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
