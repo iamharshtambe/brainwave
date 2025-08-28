19e8c96bfa6ce8ae866ec9f1b5bbcbb22b31e11a
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-28 21:31:03 +0530

    feat: fetch the products

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
+
 export default function Pagination() {
+  const [products, setPrpducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch('https://dummyjson.com/products');
+
+      const data = await response.json();
+
+      setPrpducts(data.products);
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="mx-auto flex min-h-screen items-center justify-center bg-black text-white">
       <div className="flex flex-col">
         <h1 className="p-4 text-center font-bitcount text-4xl">Pagination</h1>
+        <div>
+          {products.map((product) => (
+            <div>{product.title}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
