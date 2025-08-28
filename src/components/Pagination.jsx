e4feba421ea165e6479ad6b9f37543eafdc9485e
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-28 22:08:40 +0530

    feat: add layout for products

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,18 @@
 import { useEffect, useState } from 'react';
 
+function Product({ productObj }) {
+  return (
+    <div className="w-[200px] border border-white p-2 text-center">
+      <img
+        src={productObj.thumbnail}
+        alt="product_image"
+        className="mx-auto block w-36"
+      />
+      <p>{productObj.title}</p>
+    </div>
+  );
+}
+
 export default function Pagination() {
   const [products, setPrpducts] = useState([]);
 
@@ -16,14 +29,13 @@ export default function Pagination() {
   }, []);
 
   return (
-    <div className="mx-auto flex min-h-screen items-center justify-center bg-black text-white">
-      <div className="flex flex-col">
-        <h1 className="p-4 text-center font-bitcount text-4xl">Pagination</h1>
-        <div>
-          {products.map((product) => (
-            <div>{product.title}</div>
-          ))}
-        </div>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black pb-10 text-white">
+      <h1 className="p-4 font-bitcount text-4xl">Pagination</h1>
+
+      <div className="mx-auto flex max-w-7xl flex-wrap justify-center gap-4">
+        {products.map((product) => (
+          <Product key={product.id} productObj={product} />
+        ))}
       </div>
     </div>
   );
