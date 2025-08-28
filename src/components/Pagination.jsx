cbc20b0d0cf3495da34a012e3303473e67400df5
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-28 21:26:56 +0530

    feat: add layout for pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,3 +1,9 @@
 export default function Pagination() {
-  return <div>Pagination</div>;
+  return (
+    <div className="mx-auto flex min-h-screen items-center justify-center bg-black text-white">
+      <div className="flex flex-col">
+        <h1 className="p-4 text-center font-bitcount text-4xl">Pagination</h1>
+      </div>
+    </div>
+  );
 }
