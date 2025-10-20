a176e4818362c7fc818ebe72894aad13efc4365f
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-20 23:32:50 +0530

    feat(auth): setup deps for auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,4 +6,10 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
 });
